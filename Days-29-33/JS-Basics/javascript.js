af470dd7c73cd4eca37ed59668b7a2d6a9087d74
Matt Shrode <mattshrode@gmail.com>
2022-09-20 22:24:24 -0500

    Day 30 -  JS Practice
    Practice with arrays and objects.

--- Days-29-33/JS-Basics/javascript.js
+++ Days-29-33/JS-Basics/javascript.js
@@ -1,4 +1,13 @@
-let greetingText = "Hi my name is Matt!";
+// let greetingText = "Hi my name is Matt!";
+// let age = 36;
+// alert(greetingText);
+// alert("I am " + age + " years old.");
+
 let age = 36;
-alert(greetingText);
-alert("I am " + age + " years old.");
+let userName = 'Matt';
+let hobbies = ['Baseball', 'Gaming', 'Movies'];
+
+alert(hobbies);
+alert(hobbies[0]);
+
+let job = {title: 'Developer', place: 'Office', salary: 50000};
