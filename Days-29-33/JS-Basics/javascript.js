35b17f83211e54899d19be155bf31706ec56686a
Matt Shrode <mattshrode@gmail.com>
2022-09-23 20:43:36 -0500

    Day 31 - Variable and Function Practice
    Practice using variables and functions.

--- Days-29-33/JS-Basics/javascript.js
+++ Days-29-33/JS-Basics/javascript.js
@@ -4,10 +4,25 @@
 // alert("I am " + age + " years old.");
 
 let age = 36;
-let userName = 'Matt';
-let hobbies = ['Baseball', 'Gaming', 'Movies'];
+let userName = "Matt";
+let hobbies = ["Baseball", "Gaming", "Movies"];
 
-alert(hobbies);
-alert(hobbies[0]);
+//alert(hobbies);
+//alert(hobbies[0]);
 
-let job = {title: 'Developer', place: 'Office', salary: 50000};
+let job = {
+  title: "Developer",
+  place: "Office",
+  salary: 50000,
+};
+
+//alert(job.title);
+
+let adultYears;
+
+function calculateAdultYears(userAge) {
+    return userAge - 18;
+}
+
+adultYears = calculateAdultYears(age);
+alert(adultYears);
