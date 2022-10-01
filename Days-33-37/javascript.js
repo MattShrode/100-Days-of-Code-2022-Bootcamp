dc8eb9fd3dfbad65d8c8ae77ffc2aa3ca2376372
Matt Shrode <mattshrode@gmail.com>
2022-09-30 20:49:31 -0500

    Day 35 - Manipulating the DOM and Events
    Practice manipulating the DOM and learn about using event
    listeners.

--- Days-33-37/javascript.js
+++ Days-33-37/javascript.js
@@ -7,3 +7,56 @@
 // document.querySelector('#external-link');
 
 // anchorElement.href = 'https://academind.com';
+
+// Add an element
+// 1. Create the new element
+
+// let newAnchor = document.createElement('a');
+
+// newAnchor.href = 'https://google.com';
+// newAnchor.textContent = 'This leads to Google!';
+
+// // 2. Get access to the parent element that should hold the new element.
+
+// let firstParagraph = document.querySelector('p');
+
+// // 3. Insert the new element into the parent element content.
+
+// firstParagraph.append(newAnchor);
+
+// // Remove an element
+
+// // 1. Select the element to be removed.
+
+// let firstH1 = document.querySelector('h1');
+
+// // 2. Remove the element.
+
+// firstH1.remove(); // Works on newer browsers
+
+// // firstH1.parentElement.removeChild(firstH1); Works on older browsers
+
+// // Move elements around
+
+// firstParagraph.parentElement.append(firstParagraph);
+
+// // Inner HTML
+
+// firstParagraph.innerHTML = 'Hi! This is <strong>important!</strong>';
+
+let paragraphElement = document.querySelector('p');
+let inputElement = document.querySelector('input');
+
+paragraphElement.addEventListener('click', clicked);
+inputElement.addEventListener('input', typed);
+
+function clicked() {
+    paragraphElement.textContent = 'Clicked';
+}
+
+function typed(event) {
+    //let enteredText = inputElement.value;
+    let enteredText = event.target.value;
+    console.log(enteredText);
+    //console.log(event);
+}
