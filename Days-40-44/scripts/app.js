9f3e46f71521bf292b1b53abcd4eeea36e1ed200
Matt Shrode <mattshrode@gmail.com>
2022-10-14 21:07:14 -0500

    Day 42 - Milestone Project JS - Player Names
    Create the logic for entering and storing the player names.

--- Days-40-44/scripts/app.js
+++ Days-40-44/scripts/app.js
@@ -1,5 +1,20 @@
+let editedPlayer = 0;
+
+const players = [
+    {
+        name: '',
+        symbol: 'X'
+    },
+    {
+        name: '',
+        symbol: 'O'
+    }
+];
+
 const playerConfigOverlay = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
+const formElement = document.querySelector('form');
+const errorsOutputElement = document.getElementById('config-errors');
 
 const editPlayerOneBtn = document.getElementById('edit-player-1-btn');
 const editPlayerTwoBtn = document.getElementById('edit-player-2-btn');
@@ -10,3 +25,5 @@ editPlayerTwoBtn.addEventListener('click', openPlayerConfig);
 
 configCancelBtn.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
+
+formElement.addEventListener('submit', savePlayerConfig);
