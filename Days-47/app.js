906bfca2249edcd0bd0830c38f8e119c5a02a846
Matt Shrode <mattshrode@gmail.com>
2022-10-18 22:45:38 -0500

    Day 48 - Express Practice
    Create code with express to handle a simple form input.

--- Days-47/app.js
+++ Days-47/app.js
@@ -1,14 +1,24 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/currenttime', function(req, res) {
-    res.send("<h1>" + new Date().toISOString() + "</h1>");
-}) //localhost:3000/currenttime
+app.use(express.urlencoded({extended: false}));
 
-app.get('/', function(req, res) {
-    res.send("<h1>Hello World!</h1>");
-}) //localhost:3000/
+app.get("/currenttime", function (req, res) {
+  res.send("<h1>" + new Date().toISOString() + "</h1>");
+}); //localhost:3000/currenttime
+
+app.get("/", function (req, res) {
+  res.send(
+    '<form action="/store-user" method="POST"><label>Your Name:</label><input type="text" name="username"><button>Submit</button></form'
+  );
+}); //localhost:3000/
+
+app.post('/store-user', function(req, res) {
+    const userName = req.body.username;
+    console.log(userName);
+    res.send('<h1>Username Stored!</h1>');
+})
 
 app.listen(3000);
 
