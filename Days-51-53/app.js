0f36676429afa90975bf1bb5d0cd5eb0e7d0e606
Matt Shrode <mattshrode@gmail.com>
2022-11-04 21:55:18 -0500

    Day 52 - EJS Project Expanded
    Setup an error handler.

--- Days-51-53/app.js
+++ Days-51-53/app.js
@@ -74,7 +74,11 @@ app.get("/about", function (req, res) {
 });
 
 app.use(function (req, res) {
-  res.render('404');
+  res.render("404");
+});
+
+app.use(function (error, req, res, next) {
+  res.render('500');
 });
 
 app.listen(3000);
