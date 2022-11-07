ae6b5f4959710a8a40141dd653d66edd1ce479fa
Matt Shrode <mattshrode@gmail.com>
2022-11-06 22:17:08 -0600

    Day 54 - Advanced JS - String Literals
    Learn about constructing strings with string literals.

--- Days-54-56/functions.js
+++ Days-54-56/functions.js
@@ -1,6 +1,8 @@
 function greetUser(userName = 'User') { // Adds a default value for the parameter. Will be overridden if any value is entered.
                                         // Must come at the end, because it makes the parameter optional.
     console.log("Hi there, " + userName + "!");
+
+    console.log(`Hi there, ${userName}!`); //String Literals - More powerful way to construct strings.
 }
 
 greetUser('Matt');
