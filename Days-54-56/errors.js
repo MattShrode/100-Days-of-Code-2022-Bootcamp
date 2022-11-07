02c0a6c2ddf2c758536734a1961fec16d138c519
Matt Shrode <mattshrode@gmail.com>
2022-11-07 15:32:25 -0600

    Day 55 - Advanced JS - Scoping/Shadowing
    Learn about scoping and shadowing.

--- Days-54-56/errors.js
+++ Days-54-56/errors.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 
 function readFile() {
+    let fileData;
     try {
         const fileData = fs.readFileSync('data.json');
     } catch (error) {
         console.log(error.message);
     }
+    console.log(fileData);
     console.log('Hi there!');
 }
 
