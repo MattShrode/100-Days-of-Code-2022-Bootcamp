0f3568c95dab12c6d6a3a6914d562cc0c573d1a8
Matt Shrode <mattshrode@gmail.com>
2022-11-06 22:05:22 -0600

    Day 54 - Advanced JS - Rest and Spread
    Learn about rest parameters and the spread operator.

--- Days-54-56/functions.js
+++ Days-54-56/functions.js
@@ -4,3 +4,18 @@ function greetUser(userName = 'User') { // Adds a default value for the paramete
 }
 
 greetUser('Matt');
+
+function sumUp(...numbers) { //Rest Parameter - Tells the function to accept any number of parameters, which are then merged into an array behind the scenes.
+                             //Expects a list of values, not an array.
+    let result = 0;
+
+    for (const number of numbers) {
+        result += number; // result = result + number
+    }
+
+    return result;
+}
+
+const inputNumbers = [1, 5, 10, 20, 30];
+
+console.log(sumUp(...inputNumbers)); //Spread Operator - Using the three dots when calling the function will spread the array or object into a list of values.
