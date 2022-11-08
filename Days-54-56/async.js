65254bb6f9b327bca8b281f0f90ea3f0ddc6cd55
Matt Shrode <mattshrode@gmail.com>
2022-11-07 22:04:34 -0600

    Day 56 - Advanced JS - Promises
    Learn about promises.

--- Days-54-56/async.js
+++ Days-54-56/async.js
@@ -1,14 +1,24 @@
-const fs = require('fs');
+const fs = require("fs/promises");
 
 function readFile() {
-    let fileData;
-    
-    fs.readFile('data.txt', function(error, fileData) {
-        console.log('File parsing done.');
-        console.log(fileData.toString());
-    });
-    
-    console.log('Hi there!');
+  let fileData;
+
+  // fs.readFile('data.txt', function(error, fileData) {
+  //     console.log('File parsing done.');
+  //     console.log(fileData.toString());
+  //     //start another async tak that sends the data to a database.
+
+  // });
+
+  fs.readFile("data.txt")
+    .then(function (fileData) {
+      console.log("File parsing done.");
+      console.log(fileData.toString());
+      // return anotherAsyncOperation
+    })
+    .then(function () {}); //Promise chain
+
+  console.log("Hi there!");
 }
 
 readFile();
