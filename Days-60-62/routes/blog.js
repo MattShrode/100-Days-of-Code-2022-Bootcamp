40b45006ce50fc39cb5f69a6a77f61c218d5b049
Matt Shrode <mattshrode@gmail.com>
2022-11-11 18:40:30 -0600

    Day 62 - MySQL, Node.JS, and Express Site
    Create dynamic routes and query the database for individual pages
    for individual posts.

--- Days-60-62/routes/blog.js
+++ Days-60-62/routes/blog.js
@@ -9,7 +9,7 @@ router.get("/", function (req, res) {
 });
 
 router.get("/posts", async function (req, res) {
-    const query = `
+  const query = `
     SELECT posts.*, authors.name AS author_name FROM posts 
     INNER JOIN authors ON posts.author_id = authors.id
     `;
@@ -34,6 +34,21 @@ router.post("/posts", async function (req, res) {
     [data]
   );
   res.redirect("/posts");
+});
+
+router.get("/posts/:id", async function (req, res) {
+  const query = `
+    SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts 
+    INNER JOIN authors ON posts.author_id = authors.id
+    WHERE posts.id = ?
+    `;
+  const [posts] = await db.query(query, [req.params.id]); //Passes the id from the route.
+  
+  if (!posts || posts.length === 0) {
+    return res.status(404).render('404');
+  }
+  
+  res.render("post-detail", { post: posts[0] });
 });
 
 module.exports = router;
