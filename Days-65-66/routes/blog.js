0917f606f9d197c3e088c99227f4cac5d9630dae
Matt Shrode <mattshrode@gmail.com>
2022-11-13 17:01:40 -0600

    Day 66 - MongoDB, Node.JS, and Express
    Learn about async error handling.

--- Days-65-66/routes/blog.js
+++ Days-65-66/routes/blog.js
@@ -50,11 +50,18 @@ router.post("/posts", async function (req, res) {
 });
 
 router.get("/posts/:id", async function (req, res) {
-  const postId = req.params.id;
+  let postId = req.params.id;
+
+  try {
+    postId = new ObjectId(postId);
+  } catch (error) {
+    return res.status(404).render('404');
+  }
+
   const post = await db
     .getDb()
     .collection("posts")
-    .findOne({ _id: new ObjectId(postId) }, { projection: { summary: 0 } });
+    .findOne({ _id: postId }, { projection: { summary: 0 } });
 
   if (!post) {
     return res.status(404).render("404");
