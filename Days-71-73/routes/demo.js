50dd3218f597eb13c19c0e78a12f4a856d27cbf6
Matt Shrode <mattshrode@gmail.com>
2022-11-17 22:22:10 -0600

    Day 71 - User Sessions
    Add user login functionality.

--- Days-71-73/routes/demo.js
+++ Days-71-73/routes/demo.js
@@ -1,46 +1,73 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
+const express = require("express");
+const bcrypt = require("bcryptjs");
 
-const db = require('../data/database');
+const db = require("../data/database");
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  res.render('welcome');
+router.get("/", function (req, res) {
+  res.render("welcome");
 });
 
-router.get('/signup', function (req, res) {
-  res.render('signup');
+router.get("/signup", function (req, res) {
+  res.render("signup");
 });
 
-router.get('/login', function (req, res) {
-  res.render('login');
+router.get("/login", function (req, res) {
+  res.render("login");
 });
 
-router.post('/signup', async function (req, res) {
+router.post("/signup", async function (req, res) {
   const userData = req.body;
   const enteredEmail = userData.email;
-  const enteredConfirmEmail = userData['confirm-email'];
+  const enteredConfirmEmail = userData["confirm-email"];
   const enteredPassword = userData.password;
 
   const hashedPassword = await bcrypt.hash(enteredPassword, 12);
 
   const user = {
     email: enteredEmail,
-    password: hashedPassword
+    password: hashedPassword,
+  };
+
+  await db.getDb().collection("users").insertOne(user);
+
+  res.redirect("/login");
+});
+
+router.post("/login", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredPassword = userData.password;
+
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: enteredEmail });
+
+  if (!existingUser) {
+    console.log("Could not log in.");
+    return res.redirect("/login");
   }
 
-  await db.getDb().collection('users').insertOne(user);
+  const passwordAreEqual = await bcrypt.compare(
+    enteredPassword,
+    existingUser.password
+  );
 
-  res.redirect('/login');
+  if (!passwordAreEqual) {
+    console.log("Could not log in - password does not match.");
+    return res.redirect("/login");
+  }
+
+  console.log('User is authenticated.')
+  res.redirect('/admin');
 });
 
-router.post('/login', async function (req, res) {});
-
-router.get('/admin', function (req, res) {
-  res.render('admin');
+router.get("/admin", function (req, res) {
+  res.render("admin");
 });
 
-router.post('/logout', function (req, res) {});
+router.post("/logout", function (req, res) {});
 
 module.exports = router;
