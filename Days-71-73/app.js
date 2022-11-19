825abd2ffe4d7e465497a0e07cace6e75dfe19c0
Matt Shrode <mattshrode@gmail.com>
2022-11-18 22:02:20 -0600

    Day 71 - User Sessions
    Install and configure express-session and connect-mongodb-session.

--- Days-71-73/app.js
+++ Days-71-73/app.js
@@ -1,17 +1,34 @@
 const path = require('path');
 
 const express = require('express');
+const session = require('express-session');
+const mondgodbStore = require('connect-mongodb-session');
 
 const db = require('./data/database');
 const demoRoutes = require('./routes/demo');
 
+const MongoDBStore = mondgodbStore(session);
+
 const app = express();
+
+const sessionStore = new MongoDBStore({
+  uri: 'mongodb://localhost:21017',
+  databaseName: 'auth-demo',
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+  secret: 'super-secret',
+  resave: false,
+  saveUninitialized: false,
+  store: sessionStore
+}));
 
 app.use(demoRoutes);
 
