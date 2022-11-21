dc248fcaea9866f25ab1b47637f913db92ea4506
Matt Shrode <mattshrode@gmail.com>
2022-11-20 22:36:58 -0600

    Day 72 - User Sessions
    Use sessions in the data validation process to retain input on a
    redirect.

--- Days-71-73/routes/demo.js
+++ Days-71-73/routes/demo.js
@@ -10,7 +10,19 @@ router.get("/", function (req, res) {
 });
 
 router.get("/signup", function (req, res) {
-  res.render("signup");
+  let sessionInputData = req.session.inputData;
+  if (!sessionInputData) {
+    sessionInputData = {
+      hasError: false,
+      email: "",
+      confirmEmail: "",
+      password: "",
+    };
+  }
+
+  req.session.inputData = null;
+  
+  res.render("signup", { inputData: sessionInputData });
 });
 
 router.get("/login", function (req, res) {
@@ -33,8 +45,17 @@ router.post("/signup", async function (req, res) {
     enteredEmail != enteredConfirmEmail ||
     !enteredEmail.includes("@")
   ) {
-    console.log("Incorrect data");
-    return res.redirect("/signup");
+    req.session.inputData = {
+      hasError: true,
+      message: "Invalid input - please check your data.",
+      email: enteredEmail,
+      confirmEmail: enteredConfirmEmail,
+      password: enteredPassword
+    };
+    req.session.save(function () {
+      res.redirect("/signup");
+    });
+    return;
   }
 
   const existingUser = await db
@@ -43,8 +64,8 @@ router.post("/signup", async function (req, res) {
     .findOne({ email: enteredEmail });
 
   if (existingUser) {
-    console.log('User already exists.')
-    return res.redirect('/signup');
+    console.log("User already exists.");
+    return res.redirect("/signup");
   }
 
   const hashedPassword = await bcrypt.hash(enteredPassword, 12);
@@ -84,17 +105,17 @@ router.post("/login", async function (req, res) {
     return res.redirect("/login");
   }
 
-  req.session.user = {id: existingUser._id, email: existingUser.email};
+  req.session.user = { id: existingUser._id, email: existingUser.email };
   req.session.isAuthenticated = true;
 
-  req.session.save(function() {
+  req.session.save(function () {
     res.redirect("/admin");
   });
 });
 
 router.get("/admin", function (req, res) {
   if (!req.session.isAuthenticated) {
-    return res.status(401).render('401');
+    return res.status(401).render("401");
   }
   res.render("admin");
 });
@@ -102,7 +123,7 @@ router.get("/admin", function (req, res) {
 router.post("/logout", function (req, res) {
   req.session.user = null;
   req.session.isAuthenticated = false;
-  res.redirect('/');
+  res.redirect("/");
 });
 
 module.exports = router;
