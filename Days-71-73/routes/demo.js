bc6c514983d155647e0116f5edfd6f91a720a5bd
Matt Shrode <mattshrode@gmail.com>
2022-11-21 20:32:24 -0600

    Day 73 - User Sessions
    Add more input verification and session tracking on the signup
    and login pages.

--- Days-71-73/routes/demo.js
+++ Days-71-73/routes/demo.js
@@ -26,7 +26,17 @@ router.get("/signup", function (req, res) {
 });
 
 router.get("/login", function (req, res) {
-  res.render("login");
+  let sessionInputData = req.session.inputData;
+  if (!sessionInputData) {
+    sessionInputData = {
+      hasError: false,
+      email: "",
+      password: "",
+    };
+  }
+
+  req.session.inputData = null;
+  res.render("login", {inputData: sessionInputData});
 });
 
 router.post("/signup", async function (req, res) {
@@ -64,8 +74,17 @@ router.post("/signup", async function (req, res) {
     .findOne({ email: enteredEmail });
 
   if (existingUser) {
-    console.log("User already exists.");
-    return res.redirect("/signup");
+    req.session.inputData = {
+      hasError: true,
+      message: "User exists already.",
+      email: enteredEmail,
+      confirmEmail: enteredConfirmEmail,
+      password: enteredPassword
+    };
+    req.session.save(function(){
+      res.redirect("/signup");
+    })
+    return;
   }
 
   const hashedPassword = await bcrypt.hash(enteredPassword, 12);
@@ -91,8 +110,16 @@ router.post("/login", async function (req, res) {
     .findOne({ email: enteredEmail });
 
   if (!existingUser) {
-    console.log("Could not log in.");
-    return res.redirect("/login");
+    req.session.inputData = {
+      hasError: true,
+      message: "Could not log you in. Please check your credentials.",
+      email: enteredEmail,
+      password: enteredPassword
+    };
+    req.session.save(function(){
+      res.redirect("/login");
+    })
+    return;
   }
 
   const passwordAreEqual = await bcrypt.compare(
@@ -101,8 +128,16 @@ router.post("/login", async function (req, res) {
   );
 
   if (!passwordAreEqual) {
-    console.log("Could not log in - password does not match.");
-    return res.redirect("/login");
+    req.session.inputData = {
+      hasError: true,
+      message: "Could not log you in. Please check your credentials.",
+      email: enteredEmail,
+      password: enteredPassword
+    };
+    req.session.save(function(){
+      res.redirect("/login");
+    })
+    return;
   }
 
   req.session.user = { id: existingUser._id, email: existingUser.email};
