db0646f102861f7863ea14e58a321035100a32d0
Matt Shrode <mattshrode@gmail.com>
2022-11-21 20:53:17 -0600

    Day 73 - User Sessions
    Use res.locals to pass authorization and authentication to all
    templates.

--- Days-71-73/routes/demo.js
+++ Days-71-73/routes/demo.js
@@ -149,13 +149,11 @@ router.post("/login", async function (req, res) {
 });
 
 router.get("/admin", async function (req, res) {
-  if (!req.session.isAuthenticated) {
+  if (!res.locals.isAuth) {
     return res.status(401).render("401");
   }
-
-  const user = await db.getDb().collection('users').findOne({_id: req.session.user.id})
   
-  if(!user || !user.isAdmin) {
+  if(!res.locals.isAdmin) {
     return res.status(403).render('403');
   }
 
@@ -169,7 +167,7 @@ router.post("/logout", function (req, res) {
 });
 
 router.get("/profile", function (req, res) {
-  if (!req.session.isAuthenticated) {
+  if (!res.locals.isAuth) {
     return res.status(401).render("401");
   }
   res.render("profile");
