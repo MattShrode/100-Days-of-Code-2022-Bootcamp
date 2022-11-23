8c02fa4825c2fbd0dcef271a5c092c0bb8fd4eed
Matt Shrode <mattshrode@gmail.com>
2022-11-22 21:25:16 -0600

    Day 75 - Security
    Learn about SQL injection and methods of preventing it.

--- Days-74-75/db-injection/data/database.js
+++ Days-74-75/db-injection/data/database.js
@@ -4,8 +4,7 @@ const pool = mysql.createPool({
   host: 'localhost',
   database: 'security',
   user: 'root',
-  password: 'your-pw',
-  multipleStatements: true
+  password: 'YES'
 })
 
 module.exports = pool;
