6208e106dea1880a17028ba93f8df163fc42037e
Matt Shrode <mattshrode@gmail.com>
2022-11-28 22:05:31 -0600

    Day 80 - Online Shop Project
    Setup connection to the mongodb server.

--- Days-78-92/app.js
+++ Days-78-92/app.js
@@ -1,15 +1,23 @@
-const path = require('path'); //Handles constructing routes on all OS.
-const express = require('express');
+const path = require("path"); //Handles constructing routes on all OS.
+const express = require("express");
 
-const authRoutes = require('./routes/auth.routes');
+const db = require("./data/database");
+const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(authRoutes);
 
-app.listen(3000);
+db.connectToDatabase()
+  .then(function () {
+    app.listen(3000);
+  })
+  .catch(function (error) {
+    console.log("Failed to connect to database.");
+    console.log(error);
+  });
