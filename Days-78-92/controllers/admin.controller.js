755b452e4e69a2f19a41ff750d414d2786a5d2a9
Matt Shrode <mattshrode@gmail.com>
2022-12-27 21:20:01 -0600

    Day 87 - Online Shop Project
    Use Ajax and frontend JS to delete a product from the server and
    the DOM.

--- Days-78-92/controllers/admin.controller.js
+++ Days-78-92/controllers/admin.controller.js
@@ -66,7 +66,7 @@ async function deleteProduct(req, res, next) {
     } catch (error) {
         return next(error);
     }
-    res.redirect('/admin/products');
+    res.json({message: 'Deleted Product.'});
 }
 
 module.exports = {
