ea11b177d94ad780c944e5167361674858bcbbce
Matt Shrode <mattshrode@gmail.com>
2023-01-02 21:08:51 -0600

    Day 89 - Online Shop Project
    Begin creating a model for the shopping cart.

--- Days-78-92/app.js
+++ Days-78-92/app.js
@@ -10,6 +10,7 @@ const addCSRFTokenMiddleware = require('./middlewares/csrf-token');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
 const checkAuthStatusMiddleware = require('./middlewares/check-auth');
 const protectRoutesMiddleware = require('./middlewares/protect-routes');
+const cartMiddleware = require('./middlewares/cart');
 
 const authRoutes = require("./routes/auth.routes");
 const productsRoutes = require('./routes/products.routes');
@@ -29,6 +30,9 @@ const sessionConfig = createSessionConfig();
 
 app.use(expressSession(sessionConfig));
 app.use(csrf());
+
+app.use(cartMiddleware);
+
 app.use(addCSRFTokenMiddleware);
 app.use(checkAuthStatusMiddleware);
 
