e799544a7d93ce8f2276f84dbffea1c2df59584c
Matt Shrode <mattshrode@gmail.com>
2023-01-06 21:35:11 -0600

    Day 91 - Online Shop Project
    Set up logic to save orders in the database.

--- Days-78-92/models/order.model.js
+++ Days-78-92/models/order.model.js
@@ -1,22 +1,38 @@
-class Order {
-    //Status => pending, fullfiled, cancelled
-    constructor(cart, userData, status = 'pending', date, orderId) {
-        this.productData = cart;
-        this.userData = userData;
-        this.status = status;
-        this.date = new Date(date);
-        if(this.date) {
-            this.formattedDate = this.date.toLocaleDateString('en-US', {
-                weekday: 'short',
-                day: 'numeric',
-                month: 'long',
-                year: 'numeric',
-            })
-        }
-        this.id = orderId;
-    }
+const db = require("../data/database");
 
-    
+class Order {
+  //Status => pending, fullfiled, cancelled
+  constructor(cart, userData, status = "pending", date, orderId) {
+    this.productData = cart;
+    this.userData = userData;
+    this.status = status;
+    this.date = new Date(date);
+    if (this.date) {
+      this.formattedDate = this.date.toLocaleDateString("en-US", {
+        weekday: "short",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+      });
+    }
+    this.id = orderId;
+  }
+
+  save() {
+    if (this.id) {
+      //updating
+    } else {
+      //new order
+      const orderDocument = {
+        userData: this.userData,
+        productData: this.productData,
+        data: new Date(),
+        status: this.status,
+      };
+
+      return db.getDb().collection("orders").insertOne(orderDocument);
+    }
+  }
 }
 
 module.exports = Order;
--- Days-78-92/models/user.model.js
+++ Days-78-92/models/user.model.js
@@ -1,47 +1,57 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
+const mongodb = require("mongodb");
 
-const db = require('../data/database');
+const db = require("../data/database");
 
 class User {
-    constructor(email, password, fullname, street, city, state, postal) {
-        this.email = email,
-        this.password = password,
-        this.name = fullname,
-        this.address = {
-            street: street,
-            city: city,
-            state: state,
-            postalCode: postal,
-        };
-    }
+  constructor(email, password, fullname, street, city, state, postal) {
+    (this.email = email),
+      (this.password = password),
+      (this.name = fullname),
+      (this.address = {
+        street: street,
+        city: city,
+        state: state,
+        postalCode: postal,
+      });
+  }
 
-    async signup() {
-        const hashedPassword = await bcrypt.hash(this.password, 12);
+  async signup() {
+    const hashedPassword = await bcrypt.hash(this.password, 12);
 
-        await db.getDb().collection('users').insertOne({
-            email: this.email,
-            password: hashedPassword, //Hash!
-            name: this.name,
-            address: this.address,
-        });
-    }
+    await db.getDb().collection("users").insertOne({
+      email: this.email,
+      password: hashedPassword, //Hash!
+      name: this.name,
+      address: this.address,
+    });
+  }
 
-    getUserWithSameEmail() {
-        return db.getDb().collection('users').findOne({email: this.email});
-    }
+  static findById(userId) {
+    const uid = new mongodb.ObjectId(userId);
 
-    async existsAlready() {
-        const existingUser = await this.getUserWithSameEmail();
-        if(existingUser) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+    return db
+      .getDb()
+      .collection("users")
+      .findOne({ _id: uid }, { projection: { password: 0 } });
+  }
 
-    comparePassword(hashedPassword) {
-        return bcrypt.compare(this.password, hashedPassword);
+  getUserWithSameEmail() {
+    return db.getDb().collection("users").findOne({ email: this.email });
+  }
+
+  async existsAlready() {
+    const existingUser = await this.getUserWithSameEmail();
+    if (existingUser) {
+      return true;
+    } else {
+      return false;
     }
+  }
+
+  comparePassword(hashedPassword) {
+    return bcrypt.compare(this.password, hashedPassword);
+  }
 }
 
 module.exports = User;
