e1e3e1255d40c5e2ba224554f297e54dc005afa4
Matt Shrode <mattshrode@gmail.com>
2023-01-07 12:47:19 -0600

    Day 92 - Online Shop Project
    Display orders and manage orders as an admin.

--- Days-78-92/models/order.model.js
+++ Days-78-92/models/order.model.js
@@ -1,4 +1,5 @@
 const db = require("../data/database");
+const mongodb = require("mongodb");
 
 class Order {
   //Status => pending, fullfiled, cancelled
@@ -21,17 +22,68 @@ class Order {
   save() {
     if (this.id) {
       //updating
+      const orderId = new mongodb.ObjectId(this.id);
+      return db
+        .getDb()
+        .collection("orders")
+        .updateOne({ _id: orderId }, { set: { status: this.status } });
     } else {
       //new order
       const orderDocument = {
         userData: this.userData,
         productData: this.productData,
-        data: new Date(),
+        date: new Date(),
         status: this.status,
       };
 
       return db.getDb().collection("orders").insertOne(orderDocument);
     }
+  }
+
+  static transformOrderDocument(orderDoc) {
+    return new Order(
+      orderDoc.productData,
+      orderDoc.userData,
+      orderDoc.status,
+      orderDoc.date,
+      orderDoc._id
+    );
+  }
+
+  static transformOrderDocuments(orderDocs) {
+    return orderDocs.map(this.transformOrderDocument);
+  }
+
+  static async findAll() {
+    const orders = await db
+      .getDb()
+      .collection("orders")
+      .find()
+      .sort({ _id: -1 })
+      .toArray();
+
+    return this.transformOrderDocuments(orders);
+  }
+
+  static async findAllForUser(userId) {
+    const uid = new mongodb.ObjectId(userId);
+
+    const orders = await db.getDb()
+      .collection("orders")
+      .find({ "userData._id": uid })
+      .sort({ _id: -1 })
+      .toArray();
+
+    return this.transformOrderDocuments(orders);
+  }
+
+  static async findById(orderId) {
+    const order = await db
+      .getDb()
+      .collection("orders")
+      .findOne({ _id: new mongodb.ObjectId(orderId) });
+
+    return this.transformOrderDocuments(order);
   }
 }
 
