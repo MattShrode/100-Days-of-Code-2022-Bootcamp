ecd5832aa2bbfbb67369847e19d60c77eab217c5
Matt Shrode <mattshrode@gmail.com>
2023-01-21 10:24:00 -0600

    Introducing Vue.js
    Set up the fetch request to store an added todo in the database.

--- Days-98-99/scripts/app.js
+++ Days-98-99/scripts/app.js
@@ -1,48 +1,74 @@
 const TodosApp = {
-    data() {
-        return {
-            todos: [],
-            enteredTodoText: '',
-            editedTodoId: null,
-        };
-    },
-    methods: {
-        saveTodo(event) {
-            event.preventDefault();
+  data() {
+    return {
+      todos: [],
+      enteredTodoText: "",
+      editedTodoId: null,
+    };
+  },
+  methods: {
+    async saveTodo(event) {
+      event.preventDefault();
 
-            if(this.editedTodoId) {
-                const todoId = this.editedTodoId;
-                const todoIndex = this.todos.findIndex(function(todoItem) {
-                    return todoItem.id === todoId;
-                });
-                const updatedTodoItem = {
-                    id: this.todos[todoIndex].id,
-                    text: this.enteredTodoText
-                };
-                this.todos[todoIndex] = updatedTodoItem;
-                this.editedTodoId = null;
-            } else {
-                const newTodo = {
-                    text: this.enteredTodoText,
-                    id: new Date().toISOString()
-                };
-                this.todos.push(newTodo);
-            }
-            this.enteredTodoText = '';
-        },
-        startEditTodo(todoId) {
-            this.editedTodoId = todoId;
-            const todo = this.todos.find(function(todoItem) {
-                return todoItem.id === todoId;
-            });
-            this.enteredTodoText = todo.text;
-        },
-        deleteTodo(todoId) {
-            this.todos = this.todos.filter(function(todoItem) {
-                return todoItem.id != todoId;
-            });
+      if (this.editedTodoId) {
+        const todoId = this.editedTodoId;
+        const todoIndex = this.todos.findIndex(function (todoItem) {
+          return todoItem.id === todoId;
+        });
+        const updatedTodoItem = {
+          id: this.todos[todoIndex].id,
+          text: this.enteredTodoText,
+        };
+        this.todos[todoIndex] = updatedTodoItem;
+        this.editedTodoId = null;
+      } else {
+        let response;
+
+        try {
+          response = await fetch("http://localhost:3000/todos", {
+            method: "POST",
+            body: JSON.stringify({
+              text: this.enteredTodoText,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+        } catch (error) {
+          alert("Something went wrong!");
+          return;
         }
-    }
+
+        if (!response.ok) {
+          alert("Something went wrong!");
+          return;
+        }
+
+        const responseData = await response.json();
+        const todoId = responseData.createdTodo.id;
+
+        const newTodo = {
+          text: this.enteredTodoText,
+          id: responseData.createdTodo.id,
+        };
+
+        this.todos.push(newTodo);
+      }
+      this.enteredTodoText = "";
+    },
+    startEditTodo(todoId) {
+      this.editedTodoId = todoId;
+      const todo = this.todos.find(function (todoItem) {
+        return todoItem.id === todoId;
+      });
+      this.enteredTodoText = todo.text;
+    },
+    deleteTodo(todoId) {
+      this.todos = this.todos.filter(function (todoItem) {
+        return todoItem.id != todoId;
+      });
+    },
+  },
 };
 
-Vue.createApp(TodosApp).mount('#todos-app');
+Vue.createApp(TodosApp).mount("#todos-app");
